c9d19f58f2242c7574b6ff500ea0cf7e0fc1e406
Eng.cabdishakur <abdishakurliban54@gmail.com>
2023-09-12 13:31:28 +0300

    Initial commit

---
+++ enotif/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    'name': "External Notifications",
+    'version': '16.0.1.0.0',
+    'summary': """Receives notifications from another website""",
+    'description': """
+       Provides a way to receive notifications from another website.
+       By default this module does nothing.
+       It is used by another modules that have methods to connect and to process a certain notification.
+       For example it is used by enotif_woo module that receives notifications from WooCommerce of new customers, orders.                    
+    """,
+    'author': 'Pektsekye',
+    'category': 'Website',
+    'depends': ['base',
+                'website'
+                ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/cron.xml',                   
+        'views/view.xml'
+    ],
+    'demo': [],
+    'images': ['static/description/screenshots/ScreenShot.png'],
+    'assets': {
+        'web.assets_backend': [
+            'enotif/static/src/css/main.css',
+            'enotif/static/src/js/main.js',
+            'enotif/static/src/xml/form.xml'
+        ]
+    },               
+    'license': 'LGPL-3',
+    'application': True,    
+    'installable': True,
+    'auto_install': False,
+}
